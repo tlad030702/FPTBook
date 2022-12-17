bc1a2305a37eec1aeed4697bff1a42d6a3fb32a8
tlad030702 <103309145+tlad030702@users.noreply.github.com>
2022-12-17 14:11:59 +0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(225, MinimumLength = 7, ErrorMessage = "Max is 225 character and min is 7 character")]
+        public string Title { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string? Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly? Publish { get; set; }
+        public decimal Price { get; set; }
+        public int Rate { get; set; }
+        public string Img { get; set; }
+        public Categories Categories { get; set; }
+    }
+}
---
+++ Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required]
+        public string CategoryDescription { get; set; }
+    }
+}
