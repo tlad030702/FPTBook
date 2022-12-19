7e3cb5cdf268edc2d9c0e25a5568b2a355e3b10f
tlad030702 <103309145+tlad030702@users.noreply.github.com>
2022-12-19 13:41:01 +0700

    update nhieu thu

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FPTBook.Data;
+using FPTBook.Models;
+using FPTBook.ViewModels;
+
+namespace FPTBook.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public BooksController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        //Upload File
+        private string UploadedFile(BookViewModel model)
+        {
+            string uniFileName = null;
+            if(model.Img1 != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniFileName = Guid.NewGuid().ToString() + "_" + model.Img1.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniFileName);
+                using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Img1.CopyTo(fileStream);
+                }
+            }
+            if (model.Img2 != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniFileName = Guid.NewGuid().ToString() + "_" + model.Img2.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Img2.CopyTo(fileStream);
+                }
+            }
+            if (model.Img3 != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniFileName = Guid.NewGuid().ToString() + "_" + model.Img3.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Img3.CopyTo(fileStream);
+                }
+            }
+            return uniFileName;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            var categories = _context.Categories.ToList();
+            //Categories categories1 =new Categories();
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniFileName = UploadedFile(model);
+                Book book = new Book
+                {
+                    Title = model.Title,
+                    Price = model.Price,
+                    Rate = model.Rate,
+                    Img1 = uniFileName,
+                    Img2 = uniFileName,
+                    Img3 = uniFileName,
+                    Quality = model.Quality,
+                    CategoryId = model.CategoryId,
+                };
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BookId,Title,Price,Rate,Img1,Img2,Img3,Quality,CategoryId")] Book book)
+        {
+            if (id != book.BookId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Books.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.BookId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Books == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Books'  is null.");
+            }
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+          return _context.Books.Any(e => e.BookId == id);
+        }
+    }
+}
---
+++ ViewModels/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FPTBook.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.ViewModels
+{
+    public class BookViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int Rate { get; set; }
+        public IFormFile Img1 { get; set; }
+        public IFormFile Img2 { get; set; }
+        public IFormFile Img3 { get; set; }
+        public int Quality { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
