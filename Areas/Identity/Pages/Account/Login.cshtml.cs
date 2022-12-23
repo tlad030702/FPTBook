3ce8f6c7ac2689175b433361b214dcf444a080db
tlad030702 <103309145+tlad030702@users.noreply.github.com>
2022-12-23 23:41:33 +0700

    lan cuoi nay 23/12/2022

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -76,7 +76,7 @@ namespace FPTBook.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
